76ff621d5eb536b0a6b087c6913d241322ad52a0
Davi Coelho <davicoelho.dev@gmail.com>
2023-02-13 10:06:47 -0300

    Function representing the basic logic of the game done

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection === 'rock' && computerSelection === 'rock') {
+        return `That's a tie! ${playerSelection} ties with ${computerSelection}.`;
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return `You lost! ${playerSelection} loses to ${computerSelection}.`;
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return `You won! ${playerSelection} beats ${computerSelection}.`;
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return `You won! ${playerSelection} beats ${computerSelection}.`;
+    } else if(playerSelection === 'paper' && computerSelection === 'paper') {
+        return `That's a tie! ${playerSelection} ties with ${computerSelection}.`;
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        return `You lost! ${playerSelection} loses to ${computerSelection}.`;
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You lost! ${playerSelection} loses to ${computerSelection}.`;
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You won! ${playerSelection} beats ${computerSelection}.`;
+    } else if(playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return `That's a tie! ${playerSelection} ties with ${computerSelection}.`;
+    }
+}
+
+const playerSelection = 'Paper';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
