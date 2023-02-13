a65d54aaf6950cbbdf5f026fb50ba4e3171470e0
Davi Coelho <davicoelho.dev@gmail.com>
2023-02-13 09:35:33 -0300

    Function with basic functionality of computer choice done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world!');
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3)
+    switch(computerChoice) {
+        case 0:
+            return 'Rock';
+            break;
+        case 1:
+            return 'Paper';
+            break;
+        case 2:
+            return 'Scissors';
+            break;
+        default:
+            return 'Oops... Something went wrong.'
+    }
+}
+
